2a06d8d962fc5840d3c9756c7b920077c0613f4c
minesyouto <minesyouto8833@gmail.com>
2022-01-30 20:37:45 +0900

    Initial Commit

---
+++ SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by みねた on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
